567b2f42a245b3e4aea1714d40ec948d6488d620
Qayiya <mr.qtshotsho@gmail.com>
2022-12-11 11:15:44 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
+ * main - entry of quote
  *
- * main - entry
- *
- * Return: 1 success
+ * Return 0 success
  */
 
 int main(void)
-{
+{	
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
