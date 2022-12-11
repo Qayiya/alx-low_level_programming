fefa125725964d33d892215fff127ac099194897
Qayiya <mr.qtshotsho@gmail.com>
2022-12-11 04:35:39 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point print line in c using printf
- * Return: 0 sucess
+ * main - entry
+ *
+ * Return: 0 success
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
