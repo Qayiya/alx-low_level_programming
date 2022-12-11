0e2bdecefedda1a82568de2ff79a6de4665bf9c7
Qayiya <mr.qtshotsho@gmail.com>
2022-12-11 00:43:03 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print line using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf(%d" with proper grammer, but the outcome is a piecec of art");
+	return (0);
+}
