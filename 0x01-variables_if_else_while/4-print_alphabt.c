06e4f63572bcf2d9f976073dccda1d05cda7fcd2
Qayiya <mr.qtshotsho@gmail.com>
2022-12-12 15:52:34 -0800

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a' ; c = 'z', c++)
-		if(c != e && c != q)
+	for (c = 'a' ; c = 'z'; c++)
+	{	if(c != e && c != q)
 			putchar(c);
+	}
 	putchar('\n');
 		return (0);
 }
