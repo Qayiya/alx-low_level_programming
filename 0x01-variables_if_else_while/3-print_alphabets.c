dd063d063c1231764bdfe2cd538d5451fb9931c2
Qayiya <mr.qtshotsho@gmail.com>
2022-12-12 14:38:18 -0800

    alphabetsss

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,13 +4,13 @@
  *
  * Return: 0 (success)
  */
+
 int main(void)
 {
 	char c;
-
-	for (c = 'a' ; c = 'z'; c++)
+	for (c = 'a' ; c <= 'z' ; c++)
 		putchar(c);
-	for (c = 'A' ; c = 'Z'; c++)
+	for (c = 'A' ; c <= 'Z' ; c++)
 		putchar(c);
 	putchar('\n');
 	return (0);
