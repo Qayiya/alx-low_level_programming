f101a5253125511a8e101f05d5c5d41e4cacabf7
Qayiya <mr.qtshotsho@gmail.com>
2023-01-02 07:48:05 -0800

    last last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
+ * main - print last digit
  *
- * main - check the code
- * Return: Always 0
+ * Return: 0 success
  */
-
 int print_last_digit(int)
-
 {
-       	int r;
-	
+	int r;
+
 	print_last_digit(98);
-	
 	print_last_digit(0);
-	
 	r = print_last_digit(-1024);
-	
 	_putchar('0' + r);
-       
-	_putchar('\n');
 	return (0);
 }
