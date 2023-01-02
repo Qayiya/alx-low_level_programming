699bbac60714d9ec779083ca390f84b43f2f2ca2
Qayiya <mr.qtshotsho@gmail.com>
2023-01-01 20:07:55 -0800

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_puchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -22,6 +22,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return ('-1');
+		return (-1);
 	}
 }
