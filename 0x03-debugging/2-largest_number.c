e04d77d005077690ef0ebc62c396f566c9b958a3
Qayiya <mr.qtshotsho@gmail.com>
2023-01-03 12:41:34 -0800

    largest t2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,18 +11,19 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b)
 	{
-		largest = a;
+		if (b > c)
+			largest = a;
+		else if (a > c)
+			largest a;
+		else 
+			largest c;
 	}
-	else if (b > a && a > c)
+	else
 	{
-		largest = b;
+		if (b > c)
+			largest b;
 	}
-	else if (c > b)
-	{
-		largest = c;
-	}
-
 	return (largest);
 }
