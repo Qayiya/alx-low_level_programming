5a701331284e57c5c8bc48d7a5b2372a8c3e5a2d
Qayiya <mr.qtshotsho@gmail.com>
2023-01-03 16:02:22 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper( c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+coid print_diagonal(int n);
