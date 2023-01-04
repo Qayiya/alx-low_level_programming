ae9de9ecee4a30a22d0ba2f740460b51cf31abb8
Qayiya <mr.qtshotsho@gmail.com>
2023-01-04 03:08:01 -0800

    numbers printed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,13 @@
  * print_numbers - print seleceted numbers
  * Return: success
  */
-int print_numbers(void)
+void print_numbers(void)
 {
 	int x;
 
-	for (x <= 0; x < 10; x++)
+	for (x >= 0; x < 10; x++)
 	{
-	_putchar(x + '0');
+	putchar(x + '0');
 	}
-	_putchar('\n');
-	return (x);
+	putchar('\n');
 }
