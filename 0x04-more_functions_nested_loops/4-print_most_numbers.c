253af31b01108fd166cbbad78d03c65a6e6564b6
Qayiya <mr.qtshotsho@gmail.com>
2023-01-03 21:09:05 -0800

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,8 @@ void print_most_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		if (x != 2 && X != 4)
-			_putchar(x + '0');
+		if (x != 2 && x != 4)
+			putchar(x + '0');
 	}
-	_putchar('\n')
+	putchar('\n')
 }
