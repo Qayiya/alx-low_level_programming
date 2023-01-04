0658e0882a589a87520ebc3545d412d7a801a21d
Qayiya <mr.qtshotsho@gmail.com>
2023-01-04 03:32:39 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,17 @@ void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 0; a < 9: a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (num = 0; numb <= 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
-			if (num = 9)
+			if (num > 9)
 			{
-				_putchar((num / 10) + '0'aaaaaa0);
+				putchar((num / 10) + '0');
 			}
-			_putchar((num % 10) + '0');
+			putchar((num % 10) + '0');
 		}
 
-		_putchar('\n');
+		putchar('\n');
 	}
 }
