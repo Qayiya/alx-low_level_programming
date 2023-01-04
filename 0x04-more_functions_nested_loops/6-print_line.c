69fb07e2dc11ba43fcd31cacdc48de80c297bc47
Qayiya <mr.qtshotsho@gmail.com>
2023-01-04 03:48:18 -0800

    do you see the line?

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print line 
+ * Return: 0 success
+ */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			putchar('_');
+	}
+	putchar('\n');
+}
